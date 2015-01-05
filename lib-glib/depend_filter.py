dd26804b34047b9a79bc223841f7fe74db03b058
Simo Piiroinen <simo.piiroinen@jollamobile.com>
2015-01-05 15:13:01 +0200

    Use LGPL v2.1 license for depend_filter.py too
    The intention is that the helper scripts have the same license
    as the actual code does.

--- lib-glib/depend_filter.py
+++ lib-glib/depend_filter.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python
 # -*- encoding: utf8 -*-
 
-# ----------------------------------------------------------------------------
-# Copyright (C) 2012 Jolla Ltd.
+#######################################################################################
+#
+# Copyright (C) 2012-2014 Jolla Ltd.
 # Contact: Simo Piiroinen <simo.piiroinen@jollamobile.com>
-# License: GPLv2
-# ----------------------------------------------------------------------------
+# All rights reserved.
+#
+# This file is part of nemo keepalive package.
+#
+# You may use this file under the terms of the GNU Lesser General
+# Public License version 2.1 as published by the Free Software Foundation
+# and appearing in the file license.lgpl included in the packaging
+# of this file.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 2.1 as published by the Free Software Foundation
+# and appearing in the file license.lgpl included in the packaging
+# of this file.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+#######################################################################################
 
 import sys,os
 
